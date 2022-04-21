d1cb6c8200c899ef0bde43ff9220cbbe3060b7c0
AlanDavid-007 <alanpereiradavid@gmail.com>
2022-04-21 16:56:41 -0300

    Db pronto

---
+++ App/Db/Db.sql
@@ -0,0 +1,83 @@
+CREATE DATABASE 'padaria';
+
+CREATE TABLE `produtos` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `nome` varchar(45) DEFAULT NULL,
+    `descricao` text,
+    `quantidade` INT DEFAULT NULL,
+    `tipo` varchar(45) DEFAULT NULL,
+    `pedido_id` INT DEFAULT NULL,
+    `promocoes_id` INT DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `promocoes`(
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `nome` varchar(45) DEFAULT NULL,
+    `descricao` text,
+    `desconto` FLOAT DEFAULT NULL,
+    `dataInicio` date DEFAULT NULL,
+    `dataTermino` date DEFAULT NULL,
+    `pedido_id` INT DEFAULT NULL,
+    `pedido_pagamento_id` INT DEFAULT NULL,
+    `pedido_usuario_id` INT DEFAULT NULL,
+    `pedido_cliente_id` INT DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `usuario`(
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `nome` varchar(45) DEFAULT NULL,
+    `senha` varchar(45) DEFAULT NULL,
+    `cargo` varchar(45) DEFAULT NULL,
+    `email` varchar(45) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `categorias`(
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `nome` varchar(45) DEFAULT NULL,
+    PRIMARY KEY (`id`) `produtos_id` INT DEFAULT NULL,
+);
+
+CREATE TABLE `pedido`(
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `valor` float DEFAULT NULL,
+    `aprovapedido` boolean DEFAULT NULL,
+    `data` date DEFAULT NULL,
+    `descricao` text,
+    `valor_tele_entrega` float DEFAULT NULL,
+    `pagamento_id` INT DEFAULT NULL,
+    `usuario_id` INT DEFAULT NULL,
+    `cliente_id` INT DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `pagamento`(
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `debito` float DEFAULT NULL,
+    `credito` float DEFAULT NULL,
+    `pix` float DEFAULT NULL,
+    `dinheiro` float DEFAULT NULL,
+    `parcela` int DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `cliente`(
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `nome` varchar(45) DEFAULT NULL,
+    `senha` varchar(45) DEFAULT NULL,
+    `cidade` varchar(45) DEFAULT NULL,
+    `telefone` bigint DEFAULT NULL,
+    `endereco` varchar(45) DEFAULT NULL,
+    `email` varchar(45) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `feedback`(
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `nota` int,
+    `comentario` text,
+    `cliente_id` int DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
