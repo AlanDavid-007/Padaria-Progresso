c65cf921e1d8ba249e5a9ca62bda12022f07cd26
Alan Silva Pereira David <97121245+AlanDavid-007@users.noreply.github.com>
2022-05-31 09:55:21 -0300

    Update carrinho.php

--- Admin/carrinho.php
+++ Admin/carrinho.php
@@ -1,14 +1,19 @@
 <?php
 require __DIR__ . './Includes/header.php';
+require __DIR__ . '../vendor/autoload.php'; 
+use \App\Entity\Pedido;
+$pedidos = Pedido::getPedido();
+$Count = 'SQL COUNT(id) FROM pedido ORDER BY DESC LIMIT 1';
 ?>
-<!-- products cart -->
-<div class="container mx-auto mt-10 ">
 
+<!-- products cart -->
+<?php foreach ($pedidos as $key => $value) { ?>
+<div class="container mx-auto mt-10 ">
     <div class="flex shadow-md my-10">
       <div class="w-3/4 bg-white px-10 py-10">
         <div class="flex justify-between border-b pb-8">
           <h1 class="font-semibold text-2xl">Carrinho</h1>
-          <h2 class="font-semibold text-2xl">3 Items</h2>
+          <h2 class="font-semibold text-2xl"><?php echo $Count;?>Items</h2>
         </div>
         <div class="flex mt-10 mb-5">
           <h3 class="font-semibold text-gray-600 text-xs uppercase w-2/5">Detalhes do produto</h3>
@@ -22,111 +27,37 @@ require __DIR__ . './Includes/header.php';
               <img class="h-24" src="./Assets/risoles.png" alt="">
             </div>
             <div class="flex flex-col justify-between ml-4 flex-grow">
-              <span class="font-bold text-sm">Risóle</span>
+              <span class="font-bold text-sm"><?php echo $value['nome']->nome;?></span>
               <span class="text-red-500 text-xs">Padaria Progresso</span>
               <a href="#" class="font-semibold hover:text-red-500 text-gray-500 text-xs">Remover</a>
             </div>
           </div>
+            
+            <?php 
+          if(isset($_POST['quantity'])){
+              $quantidade = $_POST['quantity'];
+          } else if($quantidade >= 1){
+              $valor = 'R$' + $quantidade*5 + ',' + 00;
+          }
+          ?>
+            
           <div class="flex justify-center w-1/5">
             <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
             </svg>
 
-            <input class="mx-2 border text-center w-8" type="text" value="1">
+            <input class="rounded border appearance-none border-gray-400 py-2 focus:outline-none focus:border-red-500 text-base pl-3 pr-10" type="number" id="quantity" name="quantity" value="<?php echo $quantidade;?>">
+              </input>
 
             <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
               <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
             </svg>
           </div>
-          <span class="text-center w-1/5 font-semibold text-sm">R$5.00</span>
-          <span class="text-center w-1/5 font-semibold text-sm">R$5.00</span>
+          <span class="text-center w-1/5 font-semibold text-sm"><?php echo $value['valor'];?></span>
+          <span class="text-center w-1/5 font-semibold text-sm"><?php echo $valor;?></span>
         </div>
-
-        <div class="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
-          <div class="flex w-2/5"> <!-- product -->
-            <div class="w-20">
-              <img class="h-24" src="./Assets/bolos.png" alt="">
-            </div>
-            <div class="flex flex-col justify-between ml-4 flex-grow">
-              <span class="font-bold text-sm">Bolo</span>
-              <span class="text-red-500 text-xs">Padaria Progresso</span>
-              <a href="#" class="font-semibold hover:text-red-500 text-gray-500 text-xs">Remover</a>
-            </div>
-          </div>
-          <div class="flex justify-center w-1/5">
-            <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
-            </svg>
-
-            <input class="mx-2 border text-center w-8" type="text" value="1">
-
-            <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
-              <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
-            </svg>
-          </div>
-          <span class="text-center w-1/5 font-semibold text-sm">R$40.00</span>
-          <span class="text-center w-1/5 font-semibold text-sm">R$40.00</span>
-        </div>
-
-        <div class="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
-          <div class="flex w-2/5"> <!-- product -->
-            <div class="w-20">
-              <img class="h-24" src="./Assets/cucas.png" alt="">
-            </div>
-            <div class="flex flex-col justify-between ml-4 flex-grow">
-              <span class="font-bold text-sm">Cuca</span>
-              <span class="text-red-500 text-xs">Padaria Progresso</span>
-              <a href="#" class="font-semibold hover:text-red-500 text-gray-500 text-xs">Remover</a>
-            </div>
-          </div>
-          <div class="flex justify-center w-1/5">
-            <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
-            </svg>
-            <input class="mx-2 border text-center w-8" type="text" value="1">
-
-            <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
-              <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
-            </svg>
-          </div>
-          <span class="text-center w-1/5 font-semibold text-sm">R$10.00</span>
-          <span class="text-center w-1/5 font-semibold text-sm">R$10.00</span>
-        </div>
-
-        <a href="index.php" class="flex font-semibold text-indigo-600 text-sm mt-10">
-      
-          <svg class="fill-current mr-2 text-indigo-600 w-4" viewBox="0 0 448 512"><path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z"/></svg>
-          Continuar comprando
-        </a>
-      </div>
-
-      <div style="background-color: #f6f6f6;" class="w-1/4 px-8 py-10">
-        <h1 class="font-semibold text-2xl border-b pb-8">Resumo do pedido</h1>
-        <div class="flex justify-between mt-10 mb-5">
-          <span class="font-semibold text-sm uppercase">Items 3</span>
-          <span class="font-semibold text-sm">55.00R$</span>
-        </div>
-        <div>
-            <!-- Utilizar futuramente cep para calcular valor de frete -->
-          <label class="font-medium inline-block mb-3 text-sm uppercase">Envio</label>
-          <select class="block p-2 text-gray-600 w-full text-sm">
-            <option>Envio padrão - R$10.00</option>
-          </select>
-        </div>
-        <div class="py-10">
-          <label for="promo" class="font-semibold inline-block mb-3 text-sm uppercase">Código de desconto</label>
-          <input type="text" id="promo" placeholder="Enter your code" class="p-2 text-sm w-full">
-        </div>
-        <button class="bg-red-500 hover:bg-red-600 px-5 py-2 text-sm text-white uppercase">Aplicar</button>
-        <div class="border-t mt-8">
-          <div class="flex font-semibold justify-between py-6 text-sm uppercase">
-            <span>Custo total </span>
-            <span>RR$65.00</span>
-          </div>
-          <button class="bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full">Comprar</button>
-        </div>
-      </div>
-
     </div>
   </div>
-      
+<?php } ?>
 <?php
   require __DIR__ . './Includes/footer.php';
   ?>
