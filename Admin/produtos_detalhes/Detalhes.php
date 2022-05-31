337f7156afd30c137a65e491614011638a40fbaf
Alan Silva Pereira David <97121245+AlanDavid-007@users.noreply.github.com>
2022-05-31 09:55:15 -0300

    Update Detalhes.php

--- Admin/produtos_detalhes/Detalhes.php
+++ Admin/produtos_detalhes/Detalhes.php
@@ -9,8 +9,8 @@ $feedbacks = Feedback::getFeedbacks();
 $pedidos = Pedido::getPedidos();
 ?>
 
-
-<?php foreach ($produtos as $key => $value && $feedbacks as $key => $feedback) { ?>
+//Trocar posição da tag Form
+<?php foreach ($produtos as $key => $value) { ?>
   <section class="text-gray-700 body-font overflow-hidden bg-white">
   <div class="container px-5 py-24 mx-auto">
   <div class="flex">
@@ -39,7 +39,7 @@ $pedidos = Pedido::getPedidos();
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-red-500" viewBox="0 0 24 24">
               <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
             </svg>
-            <span class="text-gray-600 ml-3"><?php echo $feedback['nota'];?> Reviews</span>
+            <span class="text-gray-600 ml-3"><?php echo $value['feedback']->nota;?> Reviews</span>
           </span>
           <span class="flex ml-3 pl-3 py-2 border-l-2 border-gray-200">
             <a class="text-gray-500">
@@ -78,7 +78,7 @@ $pedidos = Pedido::getPedidos();
           <div class="flex ml-6 items-center">
             <span class="mr-3">Quantidade</span>
             <div class="relative">
-              <input class="rounded border appearance-none border-gray-400 py-2 focus:outline-none focus:border-red-500 text-base pl-3 pr-10" type="number" id="quantity" name="quantity">
+              <input class="rounded border appearance-none border-gray-400 py-2 focus:outline-none focus:border-red-500 text-base pl-3 pr-10" type="number" id="quantity" name="quantity" value="<?php echo $quantidade;?>">
               </input>
               <span class="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4" viewBox="0 0 24 24">
@@ -89,8 +89,8 @@ $pedidos = Pedido::getPedidos();
           </div>
         </div>
         <div class="flex">
-          <span class="title-font font-medium text-2xl text-gray-900"><?php echo $valor ?></span>
-          <button class="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded"><a href="">Adicionar ao Carrinho</a></button>
+          <span class="title-font font-medium text-2xl text-gray-900"><?php echo $valor?></span>
+          <button class="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded" type="submit">Adicionar ao Carrinho</button>
           // Colocar link para adicionar ao carrinho
           <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
             <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
