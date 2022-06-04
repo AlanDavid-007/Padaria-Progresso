4f19babbbf61f8e7834ec70ecca59f12c1248aaa
Alan Silva Pereira David <97121245+AlanDavid-007@users.noreply.github.com>
2022-06-04 12:51:59 -0300

    Update carrinho.php

--- Admin/carrinho.php
+++ Admin/carrinho.php
@@ -3,7 +3,7 @@ require __DIR__ . './Includes/header.php';
 require __DIR__ . '../vendor/autoload.php'; 
 use \App\Entity\Pedido;
 $pedidos = Pedido::getPedidos();
-$Counter = 'SQL COUNT(id) FROM pedido ORDER BY DESC LIMIT 1';
+$titulo = 'SQL COUNT(id) FROM pedido ORDER BY DESC LIMIT 1';
 ?>
 
 <!-- products cart -->
@@ -13,7 +13,7 @@ $Counter = 'SQL COUNT(id) FROM pedido ORDER BY DESC LIMIT 1';
       <div class="w-3/4 bg-white px-10 py-10">
         <div class="flex justify-between border-b pb-8">
           <h1 class="font-semibold text-2xl">Carrinho</h1>
-          <h2 class="font-semibold text-2xl"><?php echo $Counter;?>Items</h2>
+          <h2 class="font-semibold text-2xl"><?php echo $titulo;?>Items</h2>
         </div>
         <div class="flex mt-10 mb-5">
           <h3 class="font-semibold text-gray-600 text-xs uppercase w-2/5">Detalhes do produto</h3>
@@ -33,14 +33,22 @@ $Counter = 'SQL COUNT(id) FROM pedido ORDER BY DESC LIMIT 1';
             </div>
           </div>
             
-        
+        <?php 
+             $obPedidos->quantidade = $_POST['quantity'];
+             $quantidade = $obPedidos->quantidade;
+             $preco = $obProdutos->preco;
+             $valor = $preco;
+               if($quantidade >= 1){
+              $valor = $quantidade*$preco;
+          }
+          ?>
             
           <div class="flex justify-center w-1/5">
             <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
             </svg>
-
-            <input class="rounded border appearance-none border-gray-400 py-2 focus:outline-none focus:border-red-500 text-base pl-3 pr-10" type="number" id="quantity" name="quantity" value="<?php echo $value['quantidade']->quantidade?>">
-
+          <form method="post">
+            <input class="rounded border appearance-none border-gray-400 py-2 focus:outline-none focus:border-red-500 text-base pl-3 pr-10" type="number" id="quantity" name="quantity" value="<?php echo $value['quantidade'];?>">
+              </form>
             <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
               <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
             </svg>
