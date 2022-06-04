3fc2b2033c3419fe3664f8d82c582fcf16d647e9
Alan Silva Pereira David <97121245+AlanDavid-007@users.noreply.github.com>
2022-06-04 12:52:17 -0300

    Update cadastro_pedidos.php

--- Admin/Cadastro/cadastro_pedidos.php
+++ Admin/Cadastro/cadastro_pedidos.php
@@ -15,22 +15,31 @@ $listaProdutos = $obProdutos::getProdutos();
 // $obPagamentos = new Pagamento;
 // $obUsuarios = new Usuario;
 // $obClientes = new Cliente;
-
 // $listaCliente = $obClientes::getClientes();
 // $listaPagamento = $obPagamentos::getPagamentos();
 // $listaUsuario = $obUsuarios::getUsuarios();
 
+   $hoje = date('d/m/Y'); 
+   $obPedidos->quantidade = $_POST['quantity'];
+   $quantidade = $obPedidos->quantidade;
+   $preco = $obProdutos->preco;
+   $valor = $preco;
+   if($quantidade >= 1){
+   $valor = $quantidade*$preco;
+   $obPedidos->valor = $valor;
+   }
+                 
     // echo "<pre>"; print_r($obPedidos); echo "</pre>"; exit;
 if (isset($_POST['quantity']
 // , $_POST['pagamento_id'], $_POST['usuario_id'], $_POST['Cliente_id']
 )) {
 
-//     $obPedidos->valor = $_POST['valor'];
-//    // $obPedidos->aprovapedido = $_POST['aprovapedido'];
-    // $obPedidos->data = $_POST['data'];
-    // $obPedidos->nome = $_POST['nome'];
-    // $obPedidos->descricao = $_POST['descricao'];
-    // $obPedidos->valor_tele_entrega = $_POST['valor_tele_entrega'];
+    $obPedidos->valor = $valor;
+   // $obPedidos->aprovapedido = $_POST['aprovapedido'];
+    $obPedidos->data = $hoje;
+    $obPedidos->nome = $obProdutos->nome;
+    $obPedidos->descricao = $obProdutos->descricao;
+   // $obPedidos->valor_tele_entrega = $_POST['valor_tele_entrega'];
     $obPedidos->quantidade = $_POST['quantity'];
    // $obPedidos->pagamento_id = $_POST['pagamento_id'];]
    // $obPedidos->usuario_id = $_POST['usuario_id'];
