29c22d2ceacdbb885a82a779d81bccf9276c4b76
Alan Silva Pereira David <97121245+AlanDavid-007@users.noreply.github.com>
2022-06-06 10:47:51 -0300

    Update carrinho.php

--- Admin/carrinho.php
+++ Admin/carrinho.php
@@ -3,7 +3,9 @@ require __DIR__ . './Includes/header.php';
 require __DIR__ . '../vendor/autoload.php'; 
 use \App\Entity\Pedido;
 $pedidos = Pedido::getPedidos();
-$titulo = 'SQL COUNT(id) FROM pedido ORDER BY DESC LIMIT 1';
+$titulo = 'SELECT COUNT(\*) FROM pedido ORDER BY DESC LIMIT 1';
+$result = mysql_query($titulo);
+$registro = mysql_fetch_assoc($result);
 ?>
 
 <!-- products cart -->
@@ -13,7 +15,7 @@ $titulo = 'SQL COUNT(id) FROM pedido ORDER BY DESC LIMIT 1';
       <div class="w-3/4 bg-white px-10 py-10">
         <div class="flex justify-between border-b pb-8">
           <h1 class="font-semibold text-2xl">Carrinho</h1>
-          <h2 class="font-semibold text-2xl"><?php echo $titulo;?>Items</h2>
+          <h2 class="font-semibold text-2xl"><?php echo $registro;?>Items</h2>
         </div>
         <div class="flex mt-10 mb-5">
           <h3 class="font-semibold text-gray-600 text-xs uppercase w-2/5">Detalhes do produto</h3>
@@ -27,7 +29,7 @@ $titulo = 'SQL COUNT(id) FROM pedido ORDER BY DESC LIMIT 1';
               <img class="h-24" src="./Assets/risoles.png" alt="">
             </div>
             <div class="flex flex-col justify-between ml-4 flex-grow">
-              <span class="font-bold text-sm"><?php echo $value['nome']->nome;?></span>
+              <span class="font-bold text-sm"><?php echo $value['nome'];?></span>
               <span class="text-red-500 text-xs">Padaria Progresso</span>
               <a href="#" class="font-semibold hover:text-red-500 text-gray-500 text-xs">Remover</a>
             </div>
@@ -40,6 +42,7 @@ $titulo = 'SQL COUNT(id) FROM pedido ORDER BY DESC LIMIT 1';
              $valor = $preco;
                if($quantidade >= 1){
               $valor = $quantidade*$preco;
+              $value['valor'] = $valor;
           }
           ?>
             
@@ -47,14 +50,14 @@ $titulo = 'SQL COUNT(id) FROM pedido ORDER BY DESC LIMIT 1';
             <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
             </svg>
           <form method="post">
-            <input class="rounded border appearance-none border-gray-400 py-2 focus:outline-none focus:border-red-500 text-base pl-3 pr-10" type="number" id="quantity" name="quantity" value="<?php echo $value['quantidade'];?>">
+            <input class="rounded border appearance-none border-gray-400 py-2 focus:outline-none focus:border-red-500 text-base pl-3 pr-10" type="number" id="quantity" name="quantity" min="1" max="<?php echo $value['quantidade'];?>" value="<?php echo $value['quantidade'];?>">
               </form>
             <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
               <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
             </svg>
           </div>
           <span class="text-center w-1/5 font-semibold text-sm"><?php echo $preco;?></span>
-          <span class="text-center w-1/5 font-semibold text-sm"><?php echo $valor;?></span>
+          <span class="text-center w-1/5 font-semibold text-sm"><?php echo $value['valor'];?></span>
         </div>
     </div>
   </div>
