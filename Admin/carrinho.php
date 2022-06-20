f6c4305f07b9e9a6fadb9d2485d4e17ff2b3c13a
Alan Silva Pereira David <97121245+AlanDavid-007@users.noreply.github.com>
2022-06-20 14:37:51 -0300

    Update carrinho.php

--- Admin/carrinho.php
+++ Admin/carrinho.php
@@ -26,7 +26,7 @@ $listaPedido = $obPedidos::getPedidos();
         <h3 class="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Price</h3>
         <h3 class="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Total</h3>
       </div>
-      <?php foreach ($produtos as $key => $value) { ?>
+      <?php foreach ($pedidos as $key => $value) { ?>
         <div class="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
           <div class="flex w-2/5">
             <!-- product -->
@@ -34,19 +34,19 @@ $listaPedido = $obPedidos::getPedidos();
               <img class="h-24" src="./Assets/risoles.png" alt="">
             </div>
             <div class="flex flex-col justify-between ml-4 flex-grow">
-              <span class="font-bold text-sm"><?php echo $value['nome']; ?></span>
-              <span class="text-red-500 text-xs"><?php echo $value['tipo']->nome; ?></span>
+              <span class="font-bold text-sm"><?php echo $value['produto_id']->nome; ?></span>
+              <span class="text-red-500 text-xs"><?php echo $value['produto_id']->tipo->nome; ?></span>
               <a href="#" class="font-semibold hover:text-red-500 text-gray-500 text-xs">Remover</a>
             </div>
           </div>
           <div class="flex justify-center w-1/5">
             <span class="text-red-500 text-xs">
-            <?php $quantidade = isset($_POST['quantity']) ? $_POST['quantity'] : 1;?>
-            <?php echo $quantidade ;?>
+<!--             <?php $quantidade = isset($_POST['quantity']) ? $_POST['quantity'] : 1;?> -->
+            <?php echo $value['quantidade'] ;?>
             </span>
           </div>
-          <span class="text-center w-1/5 font-semibold text-sm"><?php echo $value['preco']; ?></span>
-          <span class="text-center w-1/5 font-semibold text-sm"><?php $valor = $value['quantidade'] * $value['preco'];?>
+          <span class="text-center w-1/5 font-semibold text-sm"><?php echo $value['produto_id']->preco; ?></span>
+          <span class="text-center w-1/5 font-semibold text-sm"><?php echo $value['valor'];?>
           <?php echo $valor; ?></span>
         </div>
         <?php } ?>
@@ -54,7 +54,7 @@ $listaPedido = $obPedidos::getPedidos();
       // vamos usar o array_sum para fazer a soma
       $saldo = array($value['valor']);
       $subtotal = array_sum($saldo);
-      $frete = $obPedidos->valor_tele_entrega;
+      $frete = 10;
       $taxa = 5;
       $total = $subtotal + $frete + $taxa;
         //  echo "<pre>"; print_r($value['valor']); echo "</pre>"; exit; 
