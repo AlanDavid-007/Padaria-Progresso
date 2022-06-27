29fdd6ec239a36ff4c40405cf5e4be86791af25e
Alan Silva Pereira David <97121245+AlanDavid-007@users.noreply.github.com>
2022-06-27 15:01:18 -0300

    Update cadastro_clientes.php

--- Cliente/Cadastro/cadastro_clientes.php
+++ Cliente/Cadastro/cadastro_clientes.php
@@ -1,25 +1,39 @@
 <?php
 require __DIR__ . '../../vendor/autoload.php';
 
-use \App\Entity\Cliente;
 use \App\Entity\Usuario;
 
 $obUsuarios = new Usuario;
-$obClientes = new Cliente;
 $usuarios = $obUsuarios::getUsuarios();
+
+/Validação do ID
+if (!isset($_GET['id'])  || !is_numeric($_GET['id'])) {
+    header('location: ../configuracoes.php?status=error');
+    exit;
+}
+
+//Consulta Vaga
+$obUsuarios = $obUsuarios::getUsuario($_GET['id']);
+// echo "<pre>"; print_r($obCurso); echo "<pre>"; exit;
+
+//Validação da Vaga
+if (!$obUsuarios instanceof $obUsuarios) {
+    header('location: ../configuracoes.php?status=error');
+    exit;
+}
 
 foreach ($usuarios as $key => $value) { 
 if (isset($_POST['primeiro_nome'], $_POST['ultimo_nome'], $_POST['senha'], $_POST['cidade'], $_POST['telefone'] $_POST['endereco'], $_POST['email'],
          $_POST['cpf'],)) { 
-    $obClientes->primeiro_nome = $_POST['primeiro_nome'];
-    $obClientes->ultimo_nome = $_POST['ultimo_nome'];
-    $obClientes->senha = $value['senha'];
-    $obClientes->cidade = $_POST['cidade'];
-    $obClientes->telefone = $_POST['telefone'];
-    $obClientes->endereco = $_POST['endereco'];
-    $obClientes->email = $_POST['email'];
-    $obClientes->cpf = $_POST['cpf'];
-    $obClientes->cadastrar();
+    $obUsuarios->primeiro_nome = $_POST['primeiro_nome'];
+    $obUsuarios->ultimo_nome = $_POST['ultimo_nome'];
+    $obUsuarios->senha = $value['senha'];
+    $obUsuarios->cidade = $_POST['cidade'];
+    $obUsuarios->telefone = $_POST['telefone'];
+    $obUsuarios->endereco = $_POST['endereco'];
+    $obUsuarios->email = $_POST['email'];
+    $obUsuarios->cpf = $_POST['cpf'];
+    $obUsuarios->atualizar();
     //   echo "<pre>"; print_r($value['preco']); echo "</pre>"; exit; 
     // echo "<pre>"; print_r($value['nome']); echo "</pre>"; exit; 
 
