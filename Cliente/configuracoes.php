a6dbe152fbcf24eb82acfb927be80244350ea9bf
Alan Silva Pereira David <97121245+AlanDavid-007@users.noreply.github.com>
2022-06-27 15:11:18 -0300

    Update configuracoes.php

--- Cliente/configuracoes.php
+++ Cliente/configuracoes.php
@@ -33,21 +33,21 @@ require __DIR__ . './Includes/header.php';
                             <input class='appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500' id='grid-text-1' type='number' placeholder='Enter email'  required
                                    value="<?php echo isset($obUsuarios->cpf) ? $obUsuarios->cpf : ''; ?>">
                         </div>
-<!--                         <div class='w-full md:w-full px-3 mb-6'>
+                        <div class='w-full md:w-full px-3 mb-6'>
                             <label class='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>Coloque sua Cidade</label>
                             <div class="flex-shrink w-full inline-block relative">
-                                <select class="block appearance-none text-gray-600 w-full bg-white border border-gray-400 shadow-inner px-4 py-2 pr-8 rounded">
+                                <select name="cidade" class="block appearance-none text-gray-600 w-full bg-white border border-gray-400 shadow-inner px-4 py-2 pr-8 rounded">
                                     <option>Escolha</option>
-                                    <option>Montenegro</option>
-                                    <option>São Sebastião do Sul</option>
-                                    <option>Pareci</option>
-                                    <option>Viamão</option>
+                                    <option value="Montenegro">Montenegro</option>
+                                    <option value="São Sebastião do Sul">São Sebastião do Sul</option>
+                                    <option value="Pareci">Pareci</option>
+                                    <option value="Viamão">Viamão</option>
                                 </select>
                                 <div class="pointer-events-none absolute top-0 mt-3  right-0 flex items-center px-2 text-gray-600">
                                     <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
                                 </div>
                             </div>
-                        </div> -->
+                        </div>
                         <div class="personal w-full border-t border-gray-400 pt-4">
                             <h2 class="text-2xl text-gray-900">Personal info:</h2>
                             <div class="flex items-center justify-between mt-4">
