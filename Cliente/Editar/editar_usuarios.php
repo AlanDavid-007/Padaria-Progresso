bfabe2c0dd4cfb75beb695c5264a6dd28fd958a0
Alan Silva Pereira David <97121245+AlanDavid-007@users.noreply.github.com>
2022-06-30 08:16:29 -0300

    Update editar_usuarios.php

--- Cliente/Editar/editar_usuarios.php
+++ Cliente/Editar/editar_usuarios.php
@@ -7,20 +7,20 @@ use \App\Entity\Usuario;
 $obUsuarios = new Usuario;
 $usuarios = $obUsuarios::getUsuarios();
 //Validação do ID
-if (!isset($_GET['id'])  || !is_numeric($_GET['id'])) {
-    header('location: ../configuracoes.php?status=error');
-    exit;
-}
+// if (!isset($_GET['id'])  || !is_numeric($_GET['id'])) {
+//     header('location: ../configuracoes.php?status=error');
+//     exit;
+// }
 
 //Consulta Vaga
-$obUsuarios = $obUsuarios::getUsuario($_GET['id']);
+// $obUsuarios = $obUsuarios::getUsuario($_GET['id']);
 // echo "<pre>"; print_r($obUsuario); echo "<pre>"; exit;
 
 //Validação da Vaga
-if (!$obUsuarios instanceof $obUsuarios) {
-    header('location: ../configuracoes.php?status=error');
-    exit;
-}
+// if (!$obUsuarios instanceof $obUsuarios) {
+//     header('location: ../configuracoes.php?status=error');
+//     exit;
+// }
 
 if (isset($_POST['primeiro_nome'], $_POST['ultimo_nome'], $_POST['senha'], $_POST['cidade'], $_POST['telefone'], $_POST['endereco'], $_POST['email'],
          $_POST['cpf'])) { 
@@ -36,11 +36,11 @@ if (isset($_POST['primeiro_nome'], $_POST['ultimo_nome'], $_POST['senha'], $_POS
     //   echo "<pre>"; print_r($value['preco']); echo "</pre>"; exit; 
     // echo "<pre>"; print_r($value['nome']); echo "</pre>"; exit; 
 
-    header('location: ../Editar/editar_usuarios.php');
+    header('location: ../../../login.php');
     exit;
 } 
 
 
-require __DIR__ . '../../../login.php';
+require __DIR__ . '../../configuracoes.php';
 ?>
 <?php ob_end_flush(); ?>
