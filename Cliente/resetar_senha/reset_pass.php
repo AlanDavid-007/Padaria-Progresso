bfcba9e15c69f55a3e3f8ea583705f30964f95c4
Alan Silva Pereira David <97121245+AlanDavid-007@users.noreply.github.com>
2022-07-19 10:22:48 -0300

    Update reset_pass.php

--- Cliente/resetar_senha/reset_pass.php
+++ Cliente/resetar_senha/reset_pass.php
@@ -7,13 +7,13 @@ require __DIR__ . '../../Includes/header_pasta.php';
         <div class="flex flex-col items-center justify-center">
 
             <div class="bg-white shadow rounded lg:w-1/3  md:w-1/2 w-full p-10 mt-16">
-                <form method="post" action="send_link.php">
-                <p tabindex="0" class="focus:outline-none text-2xl font-bold leading-6 text-gray-800">Coloque o seu email abaixo para te mandarmos o link de recuperação</p>
+                <form method="post">
+                <p tabindex="0" class="focus:outline-none text-2xl font-bold leading-6 text-gray-800">Confirme sua senha abaixo</p>
                 <div class="mt-3">
                         <label class="text-sm font-medium leading-none text-gray-800">
-                            Email
+                            Senha
                         </label>
-                        <input type="text" name="email" class="bg-gray-200 border rounded  text-xs font-medium leading-none text-gray-800 py-3 w-full pl-3 mt-2" />
+                        <input type="text" name="senha" class="bg-gray-200 border rounded  text-xs font-medium leading-none text-gray-800 py-3 w-full pl-3 mt-2" />
                     </div>
                     <div class="mt-8">
                         <input type="submit" id="submit" name="submit_email" value="Entrar" class="focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 text-sm font-semibold leading-none text-white focus:outline-none bg-indigo-700 border rounded hover:bg-indigo-600 py-4 w-full" />
